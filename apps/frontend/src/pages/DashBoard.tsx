9d16e7eb65bc1475852c64608125bd386a55a0b7
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2026-01-14 00:47:58 +0900

    feat: 대시보드 페이지에 에러 바운더리 및 오류 발생 시 쿼리 재시도 추가

--- apps/frontend/src/pages/DashBoard.tsx
+++ apps/frontend/src/pages/DashBoard.tsx
@@ -1,5 +1,8 @@
+import { ErrorFallback } from '@/components/common/ErrorFallback'
 import PurchaseFrequencyTable from '@/components/PurchaseFrequency/PurchaseFrequencyTable'
+import { useQueryErrorResetBoundary } from '@tanstack/react-query'
 import { useState, Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
 
 const DashBoard = () => {
   const [dateRange, setDateRange] = useState({
@@ -7,16 +10,19 @@ const DashBoard = () => {
     to: '2025-12-31',
   })
 
+  const { reset } = useQueryErrorResetBoundary()
+
   return (
     <main>
       <h1>쇼핑몰 구매 데이터 대시보드</h1>
       <p>
         기간: {dateRange.from} ~ {dateRange.to}
       </p>
-
-      <Suspense fallback={<div>로딩 중...</div>}>
-        <PurchaseFrequencyTable from={dateRange.from} to={dateRange.to} />
-      </Suspense>
+      <ErrorBoundary FallbackComponent={ErrorFallback} onReset={reset}>
+        <Suspense fallback={<div>로딩 중...</div>}>
+          <PurchaseFrequencyTable from={dateRange.from} to={dateRange.to} />
+        </Suspense>
+      </ErrorBoundary>
     </main>
   )
 }
