7d9438fe583ef60493f61c54a4eaf94b116aab3d
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2026-01-14 01:23:22 +0900

    feat: 가격 범위 포맷팅 유틸리티 함수 추가

---
+++ apps/frontend/src/utils/formatPriceRange.ts
@@ -0,0 +1,8 @@
+import { PurchaseFrequencyClient } from '@/apis/purchaseApi'
+
+export const formatPriceRange = (range: PurchaseFrequencyClient['range']): string => {
+  if (range.end === null) {
+    return `${range.start.toLocaleString()}원 이상`
+  }
+  return `${range.start.toLocaleString()}원 ~ ${range.end.toLocaleString()}원`
+}
