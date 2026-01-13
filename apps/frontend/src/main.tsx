83b9c683c0e37e49ab40c49329ab0d856664a6f0
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2026-01-13 23:22:56 +0900

    feat: Tanstack Query 클라이언트 설정 및 프로바이더 추가

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      throwOnError: true,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
